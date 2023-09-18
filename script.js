34844fe92c97d5e884fbf6d87be1ef25c9ce9599
rodric74 <rodric.rambosson@gmail.com>
2023-09-18 18:32:29 +0200

    reglage menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// EMAIL JS
+
 (function() {
     // Initialize EmailJS
     emailjs.init('NiRKY6SfZxvr8ysRx'); 
@@ -21,38 +23,70 @@ window.onload = function() {
     
 };
 
+//MENU 
+
 const app = (() => {
 	let body;
 	let menu;
 	let menuLinks;
+	let navItems;
 	
 	const init = () => {
-		body = document.querySelector('body');
-		menu = document.querySelector('.menu-icon');
-		menuLinks = document.querySelectorAll('.nav__list-item a, .nav__icon-item a');
-
-		applyListeners();
+	  body = document.querySelector('body');
+	  menu = document.querySelector('.menu-icon');
+	  menuLinks = document.querySelectorAll('.nav__list-item a, .nav__icon-item a');
+	  navItems = document.querySelectorAll('.nav__list-item');
+	  
+	  applyListeners();
 	}
 	
 	const applyListeners = () => {
-		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
-		menuLinks.forEach(link => {
-			link.addEventListener('click', () => {
-				body.classList.remove('nav-active');
-			});
+	  menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+	  menuLinks.forEach(link => {
+		link.addEventListener('click', () => {
+		  body.classList.remove('nav-active');
 		});
+	  });
+  
+	  navItems.forEach(item => {
+		item.addEventListener('mouseenter', handleMouseEnter);
+		item.addEventListener('mouseleave', handleMouseLeave);
+	  });
 	}
+	
+	const handleMouseEnter = (event) => {
+		const item = event.currentTarget;
+		const navContent = document.querySelector('.nav__content');
+		const itemWidth = item.offsetWidth;
+		const itemLeftOffset = item.offsetLeft;
+		const navContentWidth = navContent.offsetWidth;
+		
+		const maxWidth = navContentWidth - (itemLeftOffset + itemWidth);
+		const barWidth = itemWidth + Math.min(0, maxWidth);
+		
+		item.style.setProperty('--bar-width', `${barWidth}px`);
+	}
+	
+	
 
+	const handleMouseLeave = (event) => {
+		const item = event.currentTarget;
+		item.style.removeProperty('--bar-width');
+	}
 	
 	const toggleClass = (element, stringClass) => {
-		if(element.classList.contains(stringClass))
-			element.classList.remove(stringClass);
-		else
-			element.classList.add(stringClass);
+	  if(element.classList.contains(stringClass))
+		element.classList.remove(stringClass);
+	  else
+		element.classList.add(stringClass);
 	}
 	
 	init();
 })();
+
+
+
+//READ MORE
 
 document.querySelectorAll('.work__project-text').forEach((paragraph, index) => {
 	const words = paragraph.textContent.split(' ');
